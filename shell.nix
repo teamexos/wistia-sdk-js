9fc40b8776d337dad51ff68c878dbd1b87870fe4
Michael Clifford <cliffom@gmail.com>
2021-02-12 12:58:00 -0800

    nix integration

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/a3ab47ec9067b5f9fccda506fc8641484c3d8e73.tar.gz") {} }:
+
+with pkgs;
+
+mkShell {
+  # Sets the build inputs, i.e. what will be available in our
+  # local environment.
+  buildInputs = [
+    nodejs
+  ];
+}
